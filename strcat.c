060c4ba9cb5e8a2ef2581e0a4f5a1c9665941750
myname <myemail@gmail.com>
2023-11-12 18:04:14 +0100

    concat fun

---
+++ strcat.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strcat - This function to concatenate two strings
+ * @src: source string
+ * @dest: destination string
+ * Return: pointer to the dest string
+ */
+
+char *_strcat(char *dest, char *src)
+{
+int l = 0;
+while (dest[l] != '\0')
+{
+    l++;
+}
+int i = 0;
+while (src[i] != '\0')
+{
+    dest[l + i] = src[i];
+    i++;
+}
+dest[l + i] = '\0';
+return (dest);
+}
