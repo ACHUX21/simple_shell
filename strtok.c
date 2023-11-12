0c475915e879b875e1a291a5a83822607e7623f2
myname <myemail@gmail.com>
2023-11-12 19:37:47 +0100

    token fun

---
+++ strtok.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _strtok - This function to break string into smaller tokens
+ * @str: The string to be tokenized
+ * @delim: The delimeter
+ * Return: The pointer to 1st token, NULL if no token found
+ */
+
+char *_strtok(char *str, const char *delim)
+{
+static char *token = NULL;
+if (str != NULL)
+{
+    token = str;
+}
+else if (token == NULL)
+{
+    return (NULL);
+}
+
+char *start = token;
+token = strpbrk(token, delim);
+if (token != NULL)
+{
+    *token = '\0';
+    *token++;
+}
+return (start);
+}
